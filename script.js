619a1512e86c23679a558a9804e1dc401c7e0120
haafizhdfw <haafizhdfw13@gmail.com>
2026-09-08 17:56:56 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Tunggu sampai semua konten dimuat
-document.addEventListener("DOMContentLoaded", () => {
-  const elements = document.querySelectorAll(".container > * , .social-links a");
-
-  elements.forEach((el, index) => {
-    el.classList.add("fade-up");
-    el.style.setProperty("--delay", `${0.2 * (index + 1)}s`);
-  });
-});
